0463046fc6b27c68f7790b3fa49dce9d06cb2517
Damien Ayers <damien@omad.net>
2012-07-25 11:47:59 +1000

    Add a (blank for now) home page

--- refcollections/urls.py
+++ refcollections/urls.py
@@ -1,11 +1,14 @@
 from django.conf.urls import patterns, include, url
-
+from django.views.generic import TemplateView
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'refcollections.views.home', name='home'),
+    url(r'^$',
+        TemplateView.as_view(template_name='home.html'), name='home'),
+
     url(r'^shells/', include('shells.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
