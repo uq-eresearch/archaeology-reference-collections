ed875cb7507d678a336e31a458b17ba5dd4db94c
Damien Ayers <damien@omad.net>
2012-07-31 12:47:28 +1000

    only add debug_toolbar if it's installed

--- refcollections/settings/dev.py
+++ refcollections/settings/dev.py
@@ -10,30 +10,37 @@ TEMPLATE_DEBUG = DEBUG
 
 ############################
 
-
-
 #############################
 ### DJANGO DEBUG TOOLBAR
-MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
 
-INSTALLED_APPS += ('debug_toolbar',)
+try:
+    __import__('debug_toolbar')
+except ImportError:
+    DEBUG_TOOLBAR = False
+else:
+    DEBUG_TOOLBAR = True
 
-# Debug Toolbar configuration
-INTERNAL_IPS = ('127.0.0.1', '10.0.2.2')
-DEBUG_TOOLBAR_PANELS = (
-    'debug_toolbar.panels.version.VersionDebugPanel',
-    'debug_toolbar.panels.timer.TimerDebugPanel',
-    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
-    'debug_toolbar.panels.headers.HeaderDebugPanel',
-    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
-    'debug_toolbar.panels.template.TemplateDebugPanel',
-    'debug_toolbar.panels.sql.SQLDebugPanel',
-    'debug_toolbar.panels.signals.SignalDebugPanel',
-    'debug_toolbar.panels.logger.LoggingPanel',
-)
-DEBUG_TOOLBAR_CONFIG = {
-    'INTERCEPT_REDIRECTS': False
-}
+if DEBUG_TOOLBAR:
+    MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
+
+    INSTALLED_APPS += ('debug_toolbar',)
+
+    # Debug Toolbar configuration
+    INTERNAL_IPS = ('127.0.0.1', '10.0.2.2')
+    DEBUG_TOOLBAR_PANELS = (
+        'debug_toolbar.panels.version.VersionDebugPanel',
+        'debug_toolbar.panels.timer.TimerDebugPanel',
+        'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+        'debug_toolbar.panels.headers.HeaderDebugPanel',
+        'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+        'debug_toolbar.panels.template.TemplateDebugPanel',
+        'debug_toolbar.panels.sql.SQLDebugPanel',
+        'debug_toolbar.panels.signals.SignalDebugPanel',
+        'debug_toolbar.panels.logger.LoggingPanel',
+    )
+    DEBUG_TOOLBAR_CONFIG = {
+        'INTERCEPT_REDIRECTS': False
+    }
 
 ### END DEBUG TOOLBAR
 #############################
