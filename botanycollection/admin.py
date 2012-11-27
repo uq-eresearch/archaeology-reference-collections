808295aa1e73ba671fb47fe38a6d7e88557a7f4b
Damien Ayers <damien@omad.net>
2012-11-27 18:01:57 +1000

    bit of neatening in admin

--- botanycollection/admin.py
+++ botanycollection/admin.py
@@ -26,6 +26,54 @@ class AccessionAdmin(admin.ModelAdmin):
     list_display = ('uq_accession', 'family', 'genus', 'species', 'common_name', 'material')
 #    list_filter = ('family', 'genus')
 
+    fieldsets = (
+        (None, {
+            'fields': ('uq_accession',
+                        'material',
+                        'source',
+                        'state',
+                        'family',
+                        'subfam',
+                        'tribe',
+                        'genus',
+                        'species',
+                        'author',
+        )}),
+        ('Other', {
+            'fields': (
+                'sspna',
+                'sspau',
+                'varna',
+                'varau',
+                'cultivar',
+                'common_name',
+                'biological_synonym',
+                'famno',
+                'genno',
+                'spno',
+                'sspno',
+                'varno',
+                'detna',
+                'detdate',
+                'collector',
+                'collector_serial_no',
+                'collection_date',
+                'source_number',
+                'id_level_flag',
+        )}),
+        ('Location', {
+            'fields': (
+                'country',
+                'site_name',
+                'lat_long',
+                'altitude',
+                'notes',
+                'related_accession',
+                'grin__seed_atlas',
+                )
+        }),
+    )
+
     class Media:
         css = {
             'all': [
@@ -37,3 +85,5 @@ class AccessionAdmin(admin.ModelAdmin):
             "%sjs/jquery-1.8.2.min.js" % settings.STATIC_URL,
             "%sjs/django_admin_collapsed_inlines.js" % settings.STATIC_URL,
         ]
+
+
