ee2718d1940b2ecad7df7615922ab95c2bd08f9f
Damien Ayers <damien@omad.net>
2012-12-13 16:56:43 +1000

    use correct settings

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ import sys
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE",
-            "refcollections.settings.production")
+            "refcollections.settings.dev")
 
     from django.core.management import execute_from_command_line
 
