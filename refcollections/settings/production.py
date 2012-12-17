72166e776d005137ff47fed4ae86442ed48290cf
Damien Ayers <damien@omad.net>
2012-12-17 10:43:53 +1000

    fix deployment

--- refcollections/settings/production.py
+++ refcollections/settings/production.py
@@ -4,9 +4,9 @@ from common import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'molluscrefuat',                      # Or path to database file if using sqlite3.
-        'USER': 'molluscrefuat',                      # Not used with sqlite3.
-        'PASSWORD': 'molluscrefuat',                  # Not used with sqlite3.
+        'NAME': 'refcollections',                      # Or path to database file if using sqlite3.
+        'USER': 'refcollections',                      # Not used with sqlite3.
+        'PASSWORD': 'refcollections',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
