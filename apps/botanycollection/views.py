965b4bf5d02eaad3140be2404e8336b67644d042
Damien Ayers <damien@omad.net>
2012-12-21 12:12:11 +1000

    Display features that exist, enabling and disabling the tabs as required

--- apps/botanycollection/views.py
+++ apps/botanycollection/views.py
@@ -11,6 +11,21 @@ class AccessionDetailView(DetailView):
     model = Accession
     slug_field = 'uq_accession'
 
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super(AccessionDetailView, self).get_context_data(**kwargs)
+        # Add to the context
+        woodfeatures = hasattr(self.object, 'woodfeatures')
+        seedfeatures = hasattr(self.object, 'seedfeatures')
+        context['woodfeatures'] = woodfeatures
+        context['seedfeatures'] = seedfeatures
+
+        # which should be active by default
+        context['woodactive'] = 'active' if woodfeatures else ''
+        context['seedactive'] = 'active' if seedfeatures and not woodfeatures else ''
+
+        return context
+
 
 class HomepageView(TemplateView):
     template_name = "botanycollection/index.html"
