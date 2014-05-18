2930d9a11dc0ee910df20d18444f80f71dc6616f
Damien Ayers <damien@omad.net>
2014-05-19 09:53:32 +1000

    clean out debug toolbar config that is no longer needed

--- refcollections/settings/dev.py
+++ refcollections/settings/dev.py
@@ -22,26 +22,8 @@ else:
     DEBUG_TOOLBAR = True
 
 if DEBUG_TOOLBAR:
-    MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
-
     INSTALLED_APPS += ('debug_toolbar',)
 
-    # Debug Toolbar configuration
-    INTERNAL_IPS = ('127.0.0.1', '10.0.2.2')
-    DEBUG_TOOLBAR_PANELS = (
-        'debug_toolbar.panels.version.VersionDebugPanel',
-        'debug_toolbar.panels.timer.TimerDebugPanel',
-        'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
-        'debug_toolbar.panels.headers.HeaderDebugPanel',
-        'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
-        'debug_toolbar.panels.template.TemplateDebugPanel',
-        'debug_toolbar.panels.sql.SQLDebugPanel',
-        'debug_toolbar.panels.signals.SignalDebugPanel',
-        'debug_toolbar.panels.logger.LoggingPanel',
-    )
-    DEBUG_TOOLBAR_CONFIG = {
-        'INTERCEPT_REDIRECTS': False
-    }
 
 ### END DEBUG TOOLBAR
 #############################
